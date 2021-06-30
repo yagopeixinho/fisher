43370c480d1a3341ec88745f9ddcd565e55f7892
Yago Peixinho <yagopeixinho03@gmail.com>
2021-06-30 20:39:07 -0300

    v0.3

--- software.js
+++ software.js
@@ -1,5 +1,25 @@
 var altura = 0
 var largura = 0
+var vidas = 1
+var tempo = 15
+
+var criaMosquitoTempo = 1500
+
+var nivel = window.location.search
+
+if (nivel === '?facil') {
+
+    criaMosquitoTempo = 1500
+
+} else if (nivel === '?normal') {
+    
+    criaMosquitoTempo = 1000
+
+} else if (nivel === '?dificil') {
+
+    criaMosquitoTempo = 750
+
+}
 
 
 function ajustaTamanhoPalcoJogo() {
@@ -11,7 +31,45 @@ function ajustaTamanhoPalcoJogo() {
 
 ajustaTamanhoPalcoJogo()
 
+var cronometro = setInterval(function() {
+
+    tempo -= 1
+
+    if(tempo < 0 ) {
+        clearInterval(cronometro)
+        clearInterval(criaMosquito)
+        window.location.href = 'vitoria.html'
+
+    } else {
+
+        document.getElementById('cronometro').innerHTML = tempo
+
+    }
+
+}, 1000)
+
+
+
+
+
 function posicaoRandomica() {
+
+    if(document.getElementById('mosquito')) {
+        document.getElementById('mosquito').remove()
+
+        if(vidas > 3) {
+        
+            window.location.href= 'fim_de_jogo.html'
+
+        } else {
+            document.getElementById('v' + vidas).src = "images/coracao_vazio.png"
+
+        }
+
+        vidas++
+    }
+
+
 
     var posicaoX = Math.floor(Math.random() * window.innerWidth) - 90
     var posicaoY = Math.floor(Math.random() * window.innerHeight) - 90
@@ -28,6 +86,10 @@ function posicaoRandomica() {
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+    mosquito.onclick = function() {
+        this.remove()
+    }
     
 
     document.body.appendChild(mosquito)
